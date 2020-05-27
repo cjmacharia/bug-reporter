0ca396c5cf1a6253f1cf243e52df8f614adc9e56
cjmash <jamescollins.jcparty@gmail.com>
2020-05-28 02:24:38 +0300

    add server

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import passport from './src/passport/setup';
+// import { Strategy } from 'passport-google-oauth2'
+// import ids from './oauth'
+import auth from './src/routes/auth'
+import session from 'express-session';
+import connectMongo from 'connect-mongo'
+const MongoStore = connectMongo(session);
+require('dotenv').config()
+const app = express();
+app.use(bodyParser.json())
+//EXPRESS Session
+app.use(session ({
+  secret: 'this is a secret',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', auth)
+const port = process.env.PORT|| 8080;
+  let dbUrl = `mongodb+srv://cjmash:${process.env.DB_PASSWORD}@bugreporter-kyxqf.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+
+  mongoose.connect(dbUrl,{ 
+  useNewUrlParser: true }).then(() => console.log('connected')).catch((err) => console.log(err));
+app.listen(port, () => {
+
+})
